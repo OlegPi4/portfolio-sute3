d42e5f32ccd07e12054b86b32f26f717338fbf99
OlegPichkur <oleg1pichkur@gmail.com>
2023-11-14 09:39:19 +0200

    with

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,8 @@
-/*-----------Функции-------------*/
-
-
-
-/*---------События------------*/
 
 document.addEventListener('DOMContentLoaded', function() {
    var elems = document.querySelectorAll('.sidenav');
    var instances = M.Sidenav.init(elems);
-   console.log('burger');
- });
+});
 
 
  
